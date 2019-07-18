c3cd762270274199b43e731004c7966b4171f505
LucasPelizza <pelizza.lucas@gmail.com>
2019-07-18 17:15:52 -0300

    0.0.1 Feature: Create player view (#5)

---
+++ Basket/Basket/Modules/Club Detail/Views/PlayerRow.swift
@@ -0,0 +1,47 @@
+//
+//  PlayerRow.swift
+//  Basket
+//
+//  Created by Lucas Pelizza on 18/07/2019.
+//  Copyright © 2019 Lucas Pelizza. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlayerRow : View {
+    var player: Player
+    var body: some View {
+        HStack {
+            CircleImage(imageName: player.picture ?? "")
+                .frame(width: 80, height: 80)
+                .aspectRatio(contentMode: .fit)
+                .padding(14)
+            
+            VStack(alignment: .leading) {
+                Text("\(player.name) - \(player.country)")
+                .font(.largeTitle)
+                    .fixedSize()
+                
+                Text("Position: \(player.position)")
+                
+                HStack(alignment: .center) {
+                    Text("Weight: \(player.weight)")
+                    Spacer()
+                    Text("Height: \(player.height)")
+                    Spacer()
+                }
+
+                
+            }.frame(height: 120)
+            .padding(.trailing, 8)
+        }
+    }
+}
+
+#if DEBUG
+struct PlayerRow_Previews : PreviewProvider {
+    static var previews: some View {
+        PlayerRow(player: playerData)
+    }
+}
+#endif
