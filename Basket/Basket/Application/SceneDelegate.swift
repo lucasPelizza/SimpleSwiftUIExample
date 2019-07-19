b8142e3c19cd6939ae499b8529763233d0aca874
LucasPelizza <pelizza.lucas@gmail.com>
2019-07-19 16:14:30 -0300

    0.0.1 Chore: change the project structure (#8)

--- Basket/Basket/Application/SceneDelegate.swift
+++ Basket/Basket/Application/SceneDelegate.swift
@@ -13,23 +13,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        // Use a UIHostingController as window root view controller
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            userData.favoritePlayers.append(leaguesData[0].teams[0].players[0])
-            userData.favoriteTeams.append(leaguesData[0].teams[0])
-            userData.favoritePlayers.append(leaguesData[0].teams[1].players[0])
-            userData.favoriteTeams.append(leaguesData[0].teams[1])
-            userData.favoritePlayers.append(leaguesData[0].teams[2].players[0])
-            userData.favoriteTeams.append(leaguesData[0].teams[2])
-            userData.favoritePlayers.append(leaguesData[0].teams[3].players[0])
-            userData.favoriteTeams.append(leaguesData[0].teams[3])
             window.rootViewController = UIHostingController(rootView: TabbedNavigator())
             self.window = window
             window.makeKeyAndVisible()
